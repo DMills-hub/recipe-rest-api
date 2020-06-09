b60fd617f2223b7430e6ddcc08d11c862b7abc54
Dan Mills <maildanmills@gmail.com>
2020-06-09 16:52:51 +0100

    Add rejectunauthorised

--- util/db.js
+++ util/db.js
@@ -5,7 +5,7 @@ let pool;
 if (process.env.NODE_ENV === "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
   });
 } else {
   pool = new Pool({
